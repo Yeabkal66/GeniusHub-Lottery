ec9f5c62d30da66adeb588b2880eb67cd57c24d2
Yeabkal66 <yabugd7@gmail.com>
2026-07-21 21:55:03 +0300

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home';
+import BuyTicket from './pages/BuyTicket';
+import Payment from './pages/Payment';
+import Pending from './pages/Pending';
+
+function App() {
+  return (
+    <Router>
+      <Toaster position="top-center" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<BuyTicket />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/pending" element={<Pending />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
